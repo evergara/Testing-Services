0f9ce08343458b4bcad1255ad7f7c73e08903092
Emerson Vergara <ee-web@hotmail.com>
2022-09-01 16:01:43 -0500

    Configurando  los modelos

---
+++ src/app/models/dto/product.dto.ts
@@ -0,0 +1,7 @@
+import { Product } from '../product.model';
+
+export interface CreateProductDTO extends Omit<Product, 'id' | 'category'> {
+  categoryId: number;
+}
+
+export interface UpdateProductDTO extends Partial<CreateProductDTO> {}
---
+++ src/app/models/product.model.ts
@@ -0,0 +1,11 @@
+import { Category } from "./category.model";
+
+export interface Product {
+  id: string;
+  title: string;
+  price: number;
+  images: string[];
+  description: string;
+  category: Category;
+  taxes?: number;
+}
