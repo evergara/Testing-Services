a505fbcbd9bcce90f862d2dea57112eae50e1de4
Emerson Vergara <ee-web@hotmail.com>
2022-09-01 23:59:55 -0500

    Test for One

--- src/app/services/products.service.spec.ts
+++ src/app/services/products.service.spec.ts
@@ -12,6 +12,7 @@ import {
   generateManyProduct,
   generateOneProduct,
 } from '../models/product.model.mock';
+import { HttpStatusCode } from '@angular/common/http';
 
 fdescribe('ProductsService', () => {
   let productsService: ProductsService;
@@ -335,6 +336,116 @@ fdescribe('ProductsService', () => {
       //Assert
       expect(responseExpect).toEqual(true);
       expect(req.request.method).toEqual('DELETE');
+    });
+  });
+
+  describe('Test for One', () => {
+    it('should return a product', (doneFn) => {
+      // Arrange
+      const mockData: Product = generateOneProduct();
+      const id = '1';
+      // Act
+      productsService.getOne(id).subscribe((data) => {
+        // Assert
+        expect(data).toEqual(mockData);
+        doneFn();
+      });
+      //http config
+      const url = `${environment.API_URL}/api/v1/products/${id}`;
+      const req = httpTestingController.expectOne(url);
+      req.flush(mockData);
+      expect(req.request.method).toEqual('GET');
+    });
+
+    it('should return the right msg when the status code is 404', (doneFn) => {
+      // Arrange
+      const id = '1';
+      const msgError = '404 message';
+      const mockError = {
+        status: HttpStatusCode.NotFound,
+        statusText: msgError,
+      };
+      // Act
+      productsService.getOne(id).subscribe({
+        error: (error) => {
+          // assert
+          expect(error).toEqual('El producto no existe');
+          doneFn();
+        },
+      });
+      //http config
+      const url = `${environment.API_URL}/api/v1/products/${id}`;
+      const req = httpTestingController.expectOne(url);
+      req.flush(msgError, mockError);
+      expect(req.request.method).toEqual('GET');
+    });
+
+    it('should return the right msg when the status code is 409', (doneFn) => {
+      // Arrange
+      const id = '1';
+      const msgError = '409 message';
+      const mockError = {
+        status: HttpStatusCode.Conflict,
+        statusText: msgError,
+      };
+      // Act
+      productsService.getOne(id).subscribe({
+        error: (error) => {
+          // assert
+          expect(error).toEqual('Algo esta fallando en el server');
+          doneFn();
+        },
+      });
+      //http config
+      const url = `${environment.API_URL}/api/v1/products/${id}`;
+      const req = httpTestingController.expectOne(url);
+      req.flush(msgError, mockError);
+      expect(req.request.method).toEqual('GET');
+    });
+
+    it('should return the right msg when the status code is 401', (doneFn) => {
+      // Arrange
+      const id = '1';
+      const msgError = '409 message';
+      const mockError = {
+        status: HttpStatusCode.Unauthorized,
+        statusText: msgError,
+      };
+      // Act
+      productsService.getOne(id).subscribe({
+        error: (error) => {
+          // assert
+          expect(error).toEqual('No estas permitido');
+          doneFn();
+        },
+      });
+      //http config
+      const url = `${environment.API_URL}/api/v1/products/${id}`;
+      const req = httpTestingController.expectOne(url);
+      req.flush(msgError, mockError);
+      expect(req.request.method).toEqual('GET');
+    });
+    
+    it('should return right msg when code is undefined', (doneFn) => {
+      //Arrage
+      const productId = '4';
+      const msgError = 'Error message';
+      const mockError = {
+        status: HttpStatusCode?.BadRequest,
+        statusText: msgError,
+      };
+      //Act
+      productsService.getOne(productId).subscribe({
+        error: (error) => {
+          expect(error).toEqual('Ups algo salio mal');
+          doneFn();
+        },
+      });
+      //Assert
+      const url = `${environment.API_URL}/api/v1/products/${productId}`;
+      const req = httpTestingController.expectOne(url);
+      expect(req.request.method).toEqual('GET');
+      req.flush(msgError, mockError);
     });
   });
 });
